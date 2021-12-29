d0f2cc6eff974b1d94578ffdf0404592f2641b6b
Matt Weichel <maweiche@gmail.com>
2021-12-29 08:57:34 -0700

    added target element for bot and created setinterval function

--- cookie-bot/cookie-bot.js
+++ cookie-bot/cookie-bot.js
@@ -1,3 +1,30 @@
+//Import Dependencies
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
+
+//Initialize the driver for the browser --- Chrome
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+const driver = new webdriver.Builder().forBrowser('chrome').build();
+
+//Define target element on screen for bot -- in this case the big cookie
+driver.wait(webdriver.until.elementLocated({id: 'bigCookie'})); // tells bot to wait until element is found so an error is not returned
+const cookieEl = driver.findElement({id: 'bigCookie'}); 
+cookieEl.click();
+
+//Create an interval for bot to operate, or else bot will just perform task once
+const startInterval = () => {
+    const cookieEl = driver.findElement({id: 'bigCookie'});
+    setInterval(() => {
+        cookieEl.click();
+    }, 500); //set for every half second
+};
+
+//Create function for bot to GET website
+const init = () => {
+    driver.get('https://orteil.dashnet.org/cookieclicker/');
+    
+};
+
+init();
+
