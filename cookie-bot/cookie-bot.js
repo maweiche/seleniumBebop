d0b7a2c08e3db728e8f15b408a08ecb869fb0c41
Matt Weichel <maweiche@gmail.com>
2021-12-29 09:14:03 -0700

    created a DOM target for products to allow bot to buy upgrade

--- cookie-bot/cookie-bot.js
+++ cookie-bot/cookie-bot.js
@@ -20,10 +20,19 @@ const startInterval = () => {
     }, 500); //set for every half second
 };
 
-//Create function for bot to GET website
+//Additional Feature - 'product' buying feature for cookie clicking boosts
+//findElements returns Promise opposed to findElement which returns a ready-to-use WebElement. We do not want to assign the 'products' Promise variable, but what the Promise returns instead. 
+//The await in the variable definition forces us to be asynchronous in our funciton
+const products = await driver.findElements({className: 'product unlocked enabled'});
+
+//Create function to check if 'products' array is empty, if it isn't then click on it's last element. Why last? This game lists upgrades from worst to best, with a choice between two we will always choose the better upgrade
+
+
+//Create function for bot to initialize
 const init = () => {
-    driver.get('https://orteil.dashnet.org/cookieclicker/');
+    driver.get('https://orteil.dashnet.org/cookieclicker/'); //sets bot's target website
     driver.wait(webdriver.until.elementLocated({id: 'bigCookie'})); // tells bot to wait until element is found so an error is not returned
+    driver.wait(webdriver.until.elementLocated({className: 'product'})); // again, tells bot to wait until specified object is in DOM
     startInterval();
 };
 
