597162b8b5894599fd05dc2d27b552b595e0a51c
Matt Weichel <maweiche@gmail.com>
2021-12-30 10:01:56 -0700

    intial commit of supreme-bot with python

---
+++ supreme-bot/supreme-bot.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from requests_html import HTMLSession, AsyncHTMLSession
+import time
+
+# import config.env
+
+'''
+python supreme.py --name="North Face"
+'''
+
+base_url = 'https://www.supremenewyork.com'
